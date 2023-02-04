26af60be6f61729cf0548d58b21f50411b14a839
Abhishek0090 <palabhishek411@gmail.com>
2023-02-04 15:57:06 +0530

    added header in homescreen

---
+++ README.md
@@ -0,0 +1,6 @@
+\*\*This is a food delivery app using react native , sanity.io , tailwindcss new version -> nativewind
+
+-> installation
+
+//1. npm install -> for package installation
+//2. npm start or expo start -> for running our project on server
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,23 @@
 import React, { useLayoutEffect } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TextInput, ScrollView } from 'react-native';
 import { styled } from 'nativewind';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { ChevronDownIcon, UserIcon, AdjustmentsVerticalIcon, AdjustmentsHorizontalIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+
+
+
 
 //tailwind variable for css //-> new version 
 
-const StyledView = styled(View)
-const StyledText = styled(Text)
-const StyledImage = styled(Image)
+const ViewContent = styled(View)
+const TextContent = styled(Text)
+const ImageContent = styled(Image)
+const InputContent = styled(TextInput)
+const ScrollContent = styled(ScrollView)
 
 const HomeScreen = () => {
+
 
     const navigation = useNavigation();
 
@@ -25,26 +32,48 @@ const HomeScreen = () => {
 
 
     return (
-        <SafeAreaView>
+        <SafeAreaView className="bg-white pt-5">
             {/* Header style */}
-            <StyledView>
-                {/* <StyledText className='text-red-500'>Header</StyledText> */}
+            <ViewContent className='flex-row pb-3 items-center mx-4 space-x-2 px-2'>
 
-                <StyledImage
+                <ImageContent
                     source={{
                         uri: 'https://links.papareact.com/wru'
                     }}
-                    className="h-7 w-7 bg-gray-300 p-4 m-1 rounded-full"
+                    className="h-7 w-7 bg-gray-300 p-4  rounded-full"
                 />
-            </StyledView>
-            <StyledView>
-                <StyledText>
-                    Deliver now!
-                </StyledText>
-                <StyledText>
-                    Current Location
-                </StyledText>
-            </StyledView>
+
+                <ViewContent className='flex-1'>
+                    <TextContent className='font-bold text-gray-400 text-xs'>
+                        Deliver now!
+                    </TextContent>
+                    <TextContent className='text-xl font-bold'>
+                        Current Location
+                        <ChevronDownIcon size={20} color="#00CCBB" />
+                    </TextContent>
+                </ViewContent>
+                <UserIcon size={30} color="#00CCBB" />
+            </ViewContent>
+
+            {/* {Search} */}
+            <ViewContent className="flex-row items-center space-x-2 pb-2 mx-4 px-3">
+                <ViewContent className="flex-row space-x-2 bg-gray-200 p-2 items-center flex-1" >
+                    <MagnifyingGlassIcon size={20} color="gray" />
+                    <InputContent placeholder='Restaurants and cuisines' keyboardType='default' />
+                </ViewContent>
+                <AdjustmentsHorizontalIcon size={20} color="#00CCBB" />
+            </ViewContent>
+
+            {/* Content */}
+
+            <ScrollContent>
+
+                {/* Categories */}
+
+                {/* Featured Rows */}
+            </ScrollContent>
+
+
         </SafeAreaView>
     );
 }
