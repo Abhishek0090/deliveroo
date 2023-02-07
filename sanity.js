ca67e821d9d706d9c9d8e6ac9bf69ea6c3d9e13c
Abhishek0090 <palabhishek411@gmail.com>
2023-02-07 18:01:20 +0530

    add sanity config

--- sanity.js
+++ sanity.js
@@ -1,9 +1,19 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-const client  = sanityClient({
-    projectId : "w88dyw2k",
+const client = sanityClient({
+    projectId: "w88dyw2k",
     dataset: 'production',
     useCdn: true,
     apiVersion: "2023-02-07"
 })
+
+const builder = imageUrlBuilder(client);
+
+
+export const urlFor = (source) => builder.image(source)
+
+//Run this to add exception for localhost 3000 CORS policy 
+//sanity cors add http://localhost:3000 
+
+export default client; 
